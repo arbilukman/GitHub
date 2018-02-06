93814f33893981096a7d750bbff71261378fdd52
arbilukman <36179633+arbilukman@users.noreply.github.com>
2018-02-06 11:05:59 +0700

    latihan pertama

---
+++ lukman.js
@@ -0,0 +1,9 @@
+console.log('halo');
+console.log(1234567890)
+console.log('lukman')
+console.log('90909huhu')
+console.log('78978')
+console.log('&%**^^')
+
+console.log(__filename); //menampilkan file path
+console.log(__dirname); // menampilkan directory path
